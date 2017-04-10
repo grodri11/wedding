411ef3e00b302c8dbd7f4f473c4dfd8e8232a968
unknown <rodrg@TIAA-7641HZWF12.ad.tiaa-cref.org>
2017-04-10 14:14:00 -0400

    latest

--- server.js
+++ server.js
@@ -5,21 +5,12 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-                res.sendFile(path.join(__dirname + '/public/dashboard.html'));
-});
-
-app.get('/wedding', (req, res) => {
                 res.sendFile(path.join(__dirname + '/public/wedding.html'));
 });
 
 
 
-app.get('/dashboard', (req, res) => {
-                res.sendFile(path.join(__dirname + '/dashboard.html'));
-});
-
-
 var port = process.env.PORT || 3000;
 app.listen(port, () => {
-                console.log(`Echo running on port ${port}!`);
+                console.log(`Wedding running on port ${port}!`);
 });
